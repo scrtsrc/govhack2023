e35e3086970d68d23a856ff46accefc5ff38b112
md <cerescouters@gmail.com>
2023-08-20 04:48:07 +0800

    add icons

--- pages/events.js
+++ pages/events.js
@@ -1,17 +1,23 @@
 import eventData from "../data/events.json";
+import considerations from "../data/considerations.json";
 import { useRouter } from "next/router";
 import styles from "../styles/common.module.css";
 import Footer from "./footer";
 import Header from "./header";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Events() {
+  // Add all the solid style icons to the library
+  library.add(fas);
   const router = useRouter();
   const selectedTag = router.query.tag;
   const relevantEvents = eventData.filter((event) => event.tag === selectedTag);
-
+  console.log(considerations);
   return (
     <div className={styles.container}>
-      <Header pageTitle={selectedTag}/>
+      <Header pageTitle={selectedTag} />
       <main className={styles.main}>
         <div className={styles.grid}>
           {relevantEvents.map((e, index) => (
@@ -26,12 +32,16 @@ export default function Events() {
                 <br />
                 Location: {e.location}
                 <br />
+                {considerations.map((c, i) => (
+                  <FontAwesomeIcon key={i} icon={c.icon} />
+                ))}
+                <br />
               </p>
             </a>
           ))}
         </div>
       </main>
-      <Footer/>
+      <Footer />
       <style jsx global>{`
         html,
         body {
