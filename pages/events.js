cf451ae130e4dcd0364e38f4caa743b0743cbbbc
md <cerescouters@gmail.com>
2023-08-20 02:31:14 +0800

    fixed highlightinggit add .

--- pages/events.js
+++ pages/events.js
@@ -1,18 +1,32 @@
 import eventData from "../data/events.json";
 import { useRouter } from "next/router";
-import styles from "../styles/Events.module.css";
+import styles from "../styles/common.module.css";
 
 export default function Events() {
   const router = useRouter();
   const selectedTag = router.query.tag;
   const relevantEvents = eventData.filter((event) => event.tag === selectedTag);
 
-  console.log(relevantEvents);
-
   return (
-    <div className={styles.grid}>
-      {relevantEvents.map((e, index) => (
+    <>
+      <header>
         <a
+          href="https://perth.wa.gov.au/en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img
+            src="https://rgcopcorpweb920-cdn-endpoint.azureedge.net/-/media/Project/COP/COP/COP/Assets/Logos/City-of-Perth-Logos/COP_Brandmark_Tagline_Primary_Colour_Black.png?h=904&w=3547&rev=1c25717c54664788bd921d09d393e988&modified=20221111053706&hash=4FAB4CB4BCDE7CB401B0D54CB77158E4"
+            alt="City of Perth Logo"
+            className={styles.logo}
+          />
+        </a>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          {relevantEvents.map((e, index) => (
+            <a
               key={index}
               href={`https://perthisok.com/${e.href}`}
               className={styles.card}
@@ -25,7 +39,43 @@ export default function Events() {
                 <br />
               </p>
             </a>
-      ))}
-    </div>
+          ))}
+        </div>
+      </main>
+      <style jsx>{`
+        header {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        header img {
+          margin-left: 0.5rem;
+        }
+        header a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/common.module.css";
 import eventData from "../data/events.json";
 import Link from 'next/link';
 
 export default function Home() {
   const uniqueTags = [...new Set(eventData.map((event) => event.tag))];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,9 +15,52 @@ export default function Home() {
           href="https://rgcopcorpweb920-cdn-endpoint.azureedge.net/-/media/Themes/COP/COP/COP/COP/images/favicon-32x32.png?rev=102aca72165940d3a63748a04d47c74c&modified=20180716022216"
         />
       </Head>
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+      `}</style>
 
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          color: white;
+        }
+        * {
+          box-sizing: border-box;
+          background-color: black;
+        }
+      `}</style>
       <main>
-        <h1 className={styles.title}>Welcome to your next adventure!</h1>
+        <h1 className={styles.title}>Welcome to <strong className={styles.strong}>your</strong> next adventure!</h1>
         <p className={styles.description}>
           What would you like to do this weekend?
         </p>
@@ -51,56 +95,7 @@ export default function Home() {
         </a>
       </footer>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+     
     </div>
   );
 }
