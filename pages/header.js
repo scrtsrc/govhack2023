5a414006097a5fa5301b39de2e576302759f17c6
md <cerescouters@gmail.com>
2023-08-20 03:51:35 +0800

    fix back button

--- pages/header.js
+++ pages/header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "../styles/header.module.css";
+import Link from 'next/link';
 
 
 export default function Header({ pageTitle }) {
   return (
     <>
       <div className={styles.header}>
-        <a href="/">
-        <title className={styles.title}> &larr;</title>
-        </a>
+        <Link href="/" className={styles.noDecoration}>
+         &larr;
+        </Link>
         <h1>{`${pageTitle} EVENTS`} </h1>
       </div>
     </>
